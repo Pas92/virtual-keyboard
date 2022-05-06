987a48f3d2cd36d87a708a32b5b3244255e97521
Pas92 <pavelburdovitsin92@gmail.com>
2022-05-06 15:16:41 +0300

    feat: add part of keyboard data

---
+++ src/data/keyboard-data.js
@@ -0,0 +1,509 @@
+export const keyboardData = [
+  [
+    {
+      eng: {
+        char: '`',
+        shiftChar: '~',
+        capsChar: '`',
+        shiftCapsChar: '~',
+      },
+      rus: {
+        char: 'ё',
+        shiftChar: 'Ё',
+        capsChar: 'Ё',
+        shiftCapsChar: 'ё',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'Backquote',
+    },
+
+    {
+      eng: {
+        char: '1',
+        shiftChar: '!',
+        capsChar: '1',
+        shiftCapsChar: '!',
+      },
+      rus: {
+        char: '1',
+        shiftChar: '!',
+        capsChar: '1',
+        shiftCapsChar: '!',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'Digit1',
+    },
+
+    {
+      eng: {
+        char: '2',
+        shiftChar: '@',
+        capsChar: '2',
+        shiftCapsChar: '@',
+      },
+      rus: {
+        char: '2',
+        shiftChar: '"',
+        capsChar: '2',
+        shiftCapsChar: '"',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'Digit2',
+    },
+
+    {
+      eng: {
+        char: '3',
+        shiftChar: '#',
+        capsChar: '3',
+        shiftCapsChar: '#',
+      },
+      rus: {
+        char: '3',
+        shiftChar: '№',
+        capsChar: '3',
+        shiftCapsChar: '№',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'Digit3',
+    },
+
+    {
+      eng: {
+        char: '4',
+        shiftChar: '$',
+        capsChar: '4',
+        shiftCapsChar: '$',
+      },
+      rus: {
+        char: '4',
+        shiftChar: ';',
+        capsChar: '4',
+        shiftCapsChar: ';',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'Digit4',
+    },
+
+    {
+      eng: {
+        char: '5',
+        shiftChar: '%',
+        capsChar: '5',
+        shiftCapsChar: '%',
+      },
+      rus: {
+        char: '5',
+        shiftChar: '%',
+        capsChar: '5',
+        shiftCapsChar: '%',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'Digit5',
+    },
+
+    {
+      eng: {
+        char: '6',
+        shiftChar: '^',
+        capsChar: '6',
+        shiftCapsChar: '^',
+      },
+      rus: {
+        char: '6',
+        shiftChar: ':',
+        capsChar: '6',
+        shiftCapsChar: ':',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'Digit6',
+    },
+
+    {
+      eng: {
+        char: '7',
+        shiftChar: '&',
+        capsChar: '7',
+        shiftCapsChar: '&',
+      },
+      rus: {
+        char: '7',
+        shiftChar: '?',
+        capsChar: '7',
+        shiftCapsChar: '?',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'Digit7',
+    },
+
+    {
+      eng: {
+        char: '8',
+        shiftChar: '*',
+        capsChar: '8',
+        shiftCapsChar: '*',
+      },
+      rus: {
+        char: '8',
+        shiftChar: '*',
+        capsChar: '8',
+        shiftCapsChar: '*',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'Digit8',
+    },
+
+    {
+      eng: {
+        char: '9',
+        shiftChar: '(',
+        capsChar: '9',
+        shiftCapsChar: '(',
+      },
+      rus: {
+        char: '9',
+        shiftChar: '(',
+        capsChar: '9',
+        shiftCapsChar: '(',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'Digit9',
+    },
+
+    {
+      eng: {
+        char: '0',
+        shiftChar: ')',
+        capsChar: '0',
+        shiftCapsChar: ')',
+      },
+      rus: {
+        char: '0',
+        shiftChar: ')',
+        capsChar: '0',
+        shiftCapsChar: ')',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'Digit0',
+    },
+
+    {
+      eng: {
+        char: '-',
+        shiftChar: '_',
+        capsChar: '-',
+        shiftCapsChar: '_',
+      },
+      rus: {
+        char: '-',
+        shiftChar: '_',
+        capsChar: '-',
+        shiftCapsChar: '_',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'Minus',
+    },
+
+    {
+      eng: {
+        char: '=',
+        shiftChar: '+',
+        capsChar: '=',
+        shiftCapsChar: '+',
+      },
+      rus: {
+        char: '=',
+        shiftChar: '+',
+        capsChar: '=',
+        shiftCapsChar: '+',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'Equal',
+    },
+
+    {
+      eng: {
+        char: 'Backspace',
+        shiftChar: 'Backspace',
+        capsChar: 'Backspace',
+        shiftCapsChar: 'Backspace',
+      },
+      rus: {
+        char: 'Backspace',
+        shiftChar: 'Backspace',
+        capsChar: 'Backspace',
+        shiftCapsChar: 'Backspace',
+      },
+      isFunc: true,
+      isRow: false,
+      code: 'Backspace',
+    },
+  ],
+
+  [
+    {
+      eng: {
+        char: 'Tab',
+        shiftChar: 'Tab',
+        capsChar: 'Tab',
+        shiftCapsChar: 'Tab',
+      },
+      rus: {
+        char: 'Tab',
+        shiftChar: 'Tab',
+        capsChar: 'Tab',
+        shiftCapsChar: 'Tab',
+      },
+      isFunc: true,
+      isRow: false,
+      code: 'Tab',
+    },
+
+    {
+      eng: {
+        char: 'q',
+        shiftChar: 'Q',
+        capsChar: 'Q',
+        shiftCapsChar: 'q',
+      },
+      rus: {
+        char: 'й',
+        shiftChar: 'Й',
+        capsChar: 'Й',
+        shiftCapsChar: 'й',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'KeyQ',
+    },
+
+    {
+      eng: {
+        char: 'w',
+        shiftChar: 'W',
+        capsChar: 'W',
+        shiftCapsChar: 'w',
+      },
+      rus: {
+        char: 'ц',
+        shiftChar: 'Ц',
+        capsChar: 'Ц',
+        shiftCapsChar: 'ц',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'KeyW',
+    },
+
+    {
+      eng: {
+        char: 'e',
+        shiftChar: 'E',
+        capsChar: 'E',
+        shiftCapsChar: 'e',
+      },
+      rus: {
+        char: 'у',
+        shiftChar: 'У',
+        capsChar: 'У',
+        shiftCapsChar: 'у',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'KeyE',
+    },
+
+    {
+      eng: {
+        char: 'r',
+        shiftChar: 'R',
+        capsChar: 'R',
+        shiftCapsChar: 'r',
+      },
+      rus: {
+        char: 'к',
+        shiftChar: 'К',
+        capsChar: 'К',
+        shiftCapsChar: 'к',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'KeyR',
+    },
+
+    {
+      eng: {
+        char: 't',
+        shiftChar: 'T',
+        capsChar: 'T',
+        shiftCapsChar: 't',
+      },
+      rus: {
+        char: 'е',
+        shiftChar: 'Е',
+        capsChar: 'Е',
+        shiftCapsChar: 'е',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'KeyT',
+    },
+
+    {
+      eng: {
+        char: 'y',
+        shiftChar: 'Y',
+        capsChar: 'Y',
+        shiftCapsChar: 'y',
+      },
+      rus: {
+        char: 'н',
+        shiftChar: 'Н',
+        capsChar: 'Н',
+        shiftCapsChar: 'н',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'KeyY',
+    },
+
+    {
+      eng: {
+        char: 'u',
+        shiftChar: 'U',
+        capsChar: 'U',
+        shiftCapsChar: 'u',
+      },
+      rus: {
+        char: 'г',
+        shiftChar: 'Г',
+        capsChar: 'Г',
+        shiftCapsChar: 'г',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'KeyU',
+    },
+
+    {
+      eng: {
+        char: 'i',
+        shiftChar: 'I',
+        capsChar: 'I',
+        shiftCapsChar: 'i',
+      },
+      rus: {
+        char: 'ш',
+        shiftChar: 'Ш',
+        capsChar: 'Ш',
+        shiftCapsChar: 'ш',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'KeyI',
+    },
+
+    {
+      eng: {
+        char: 'o',
+        shiftChar: 'O',
+        capsChar: 'O',
+        shiftCapsChar: 'o',
+      },
+      rus: {
+        char: 'щ',
+        shiftChar: 'Щ',
+        capsChar: 'Щ',
+        shiftCapsChar: 'щ',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'KeyO',
+    },
+
+    {
+      eng: {
+        char: 'p',
+        shiftChar: 'P',
+        capsChar: 'P',
+        shiftCapsChar: 'p',
+      },
+      rus: {
+        char: 'з',
+        shiftChar: 'З',
+        capsChar: 'З',
+        shiftCapsChar: 'з',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'KeyP',
+    },
+
+    {
+      eng: {
+        char: '[',
+        shiftChar: '{',
+        capsChar: '[',
+        shiftCapsChar: '{',
+      },
+      rus: {
+        char: 'х',
+        shiftChar: 'Х',
+        capsChar: 'Х',
+        shiftCapsChar: 'х',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'BracketLeft',
+    },
+
+    {
+      eng: {
+        char: ']',
+        shiftChar: '}',
+        capsChar: ']',
+        shiftCapsChar: '}',
+      },
+      rus: {
+        char: 'ъ',
+        shiftChar: 'Ъ',
+        capsChar: 'Ъ',
+        shiftCapsChar: 'ъ',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'BracketRight',
+    },
+
+    {
+      eng: {
+        char: 'Del',
+        shiftChar: 'Del',
+        capsChar: 'Del',
+        shiftCapsChar: 'Del',
+      },
+      rus: {
+        char: 'Del',
+        shiftChar: 'Del',
+        capsChar: 'Del',
+        shiftCapsChar: 'Del',
+      },
+      isFunc: false,
+      isRow: false,
+      code: 'Delete',
+    },
+  ],
+];
